97193bc4b0c471b1091f147531f1142792640f4d
Jonathan Leibovich <leibo023@umn.edu>
2020-02-15 19:38:08 -0600

    Start of Progress bar and reorganize everything

--- Main.py
+++ Main.py
@@ -1,18 +1,22 @@
-import math
 import pygame
-s_width = 1250
+#Variables
+s_width = 1250 #Screen Size
 s_height = 750
-win =pygame.display.set_mode((s_width,s_height))
-pygame.display.set_caption("Sandwich Time")
-
-run= True
-
 x=50
 y=690
-b_width=100
+b_width=100 #Background tiles
 b_height=100
-win.fill((0,0,0))
+p_height=225 #progress bar
+p_width=55
+progress=200
+
+win =pygame.display.set_mode((s_width,s_height))
+pygame.display.set_caption("Sandwich Time")
+pygame.init()
+run= True
+
 #Background
+win.fill((0,0,0))#Black Background
 for col in range(0,s_width,b_width*2):#This is for the first set of rows and cols
     for row in range(0,s_height,b_height*2):
         pygame.draw.rect(win,(255,255,255),(col,row,b_width,b_height))
@@ -20,14 +24,17 @@ for col in range(b_width,s_width,b_width*2):#Second set of rows and cols
     for row in range(b_height,s_height,b_height*2):
         pygame.draw.rect(win,(255,255,255),(col,row,b_width,b_height))
 
-print("done")
+pygame.draw.rect(win,(0,0,0),(0,0,250,s_height))#Black left Column
+pygame.draw.rect(win,(0,0,255),(125-(p_width//2),s_height-p_height-5,p_width,p_height))#Progress Bar
+
+
 pygame.display.update()
 
-
-
-#Keep (makes the game not break)
 while  run:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
+
+
+    pygame.display.update()
 pygame.quit()
